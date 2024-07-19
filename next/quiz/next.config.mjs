fa0d1e0784adf9ff495b88957ecd801604a1733b
Gustavo <123191793+Gus41@users.noreply.github.com>
2024-07-19 16:32:25 +0000

    .

--- next/quiz/next.config.mjs
+++ next/quiz/next.config.mjs
@@ -1,20 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async headers() {
-      return [
-          {
-              // matching all API routes
-              source: "/api/:path*",
-              headers: [
-                  { key: "Access-Control-Allow-Credentials", value: "true" },
-                  { key: "Access-Control-Allow-Origin", value: "https://react-js-next-js-hgru-6kzg7dk8n-gus41s-projects.vercel.app" }, // replace this your actual origin
-                  { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-                  { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-              ]
-          }
-      ]
-  }
 };
 
 export default nextConfig;
